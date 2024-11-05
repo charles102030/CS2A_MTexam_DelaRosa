233f333579ee9f7813591946d308baeba74f09df
charles102030 <delarosacharles201@gmail.com>
2024-11-05 10:29:14 +0800

    Create userClass.js

---
+++ userClass.js
@@ -0,0 +1,5 @@
+let subjectTitle = "Data Structures and Algorithms";
+let classSchedule = "8:00 AM - 9:00 AM, Tuesday and Thursday";
+let classroom = "Comp lab";
+let classInstructor = "Vitruvius John Barayuga";
+let studentName = "Charles Dela Rosa";
